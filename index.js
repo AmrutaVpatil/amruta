9782dae7a6eda4a7e87b139cc522a092dc6ac6c9
AmrutaVpatil <patilamruta9187@gmail.com>
2025-02-04 10:44:29 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,106 +1,173 @@
-const express = require('express');
-const mongodb = require('mongodb');
+var express=require('express')
+const cors = require('cors');
+var app=express()
+var mongodb=require('mongodb')
+const mongoClient=mongodb.MongoClient
 
-const app = express();
-const MongoClient = mongodb.MongoClient;
+ app.use(cors()); 
+//1. establish the connection
+let dburl='mongodb+srv://Sathish:sathish3210@mongofristdb.jvmrx1b.mongodb.net/'
+app.use(express.json())
+app.get('/', async function(req,res)
 
-const dbUrl = 'mongodb+srv://demoproject:demoproject@cluster0.pj2bg.mongodb.net/'
-const dbName = 'amity';
-
-app.use(express.json());
-
-let client;
-
-// Inialize mongoDB connection Once
-async function connectDB() {
-    if(!client) {
-        client = await MongoClient.connect(dbUrl);
-        console.log('Connected to mongodb')
-    }
-    return client.db(dbName);
-}
-
-// Get All Users
-app.get('/', async(req, res)=>{
-    try{
-        const db = await connectDB();
-        const users = await db.collection('userDetails').find().toArray();
-        res.json({message: 'Displaying all records', users});
-    } 
-    catch(error)
+{
+    const client= await mongoClient.connect(dburl)
+    try
     {
-        console.error(error);
-        res.status(500).json({message: 'Internal Server Error'});
+       let db= await client.db('amity')
+       let user= await db.collection('userDetails').find().toArray()
+       res.json({
+        message:'Displaying all records',
+        user
+       })
     }
-});
+       catch(error)
+       {
+        console.log(error);
+       }
+     
+   finally{
+    client.close()
 
-// Insert New Record
-app.post('/', async(req, res)=>{
-    try{
-        const db = await connectDB();
-        const result = await db.collection('userDetails').insertOne(req.body);
-        res.json({message: 'Record Inserted', insertedId: result.insertedId});
+
+   }
+
+}) 
+
+app.post('/register', async function(req,res){
+console.log('Reqe Body',req.body);
+    const client= await mongoClient.connect(dburl)
+    try
+    {
+        const client=await mongoClient.connect(dburl);
+       let db= await client.db('Sathish')
+       let user= await db.collection('UserDetails').insertOne(req.body)
+       res.json({
+        message:'Record Inserted'
+        
+       })
     }
-    catch(error){
-        console.error(error);
-        res.status(500).json({message: 'Internal Server Error'});
+       catch(error)
+       {
+        console.log(error);
+       }
+    
+   finally{
+    client.close()
+
+s
+   }
+
+})
+app.get('/fetch/:num', async function(req,res)
+
+{
+    const client= await mongoClient.connect(dburl)
+    const id =parseInt(req.params.num)
+    try
+    {
+       let db= await client.db('amity')
+       let user= await db.collection('userDetails').findOne({id:id})
+       if(user)
+       {
+       res.json({
+        message:'Record Found',
+        user
+        
+       })
     }
-});
-
-// Fetch User by ID
-app.get('/fetch/:id', async(req, res)=>{
-    try {
-        const db = await connectDB();
-        const id = parseInt(req.params.id);
-        const user = await db.collection('userDetails').findOne({ id });
-
-        if(user)
-            {
-            res.json({ message: 'Record Found', user });
-        }else{
-            res.status(404).json({ message: 'Record Not Found' });
-        }
-    }catch(error){
-        console.error(error);
-        res.status(500).json({ message: 'Internal Server Error' });
+    else{
+        res.json({
+            message:'record not found'
+        })
     }
-});
-
-// Update User by Name
-app.put('/update/:name', async(req, res)=>{
-    try{
-        const db = await connectDB();
-        const name = req.params.name;
-        const updatedData = { $set: req.body };
-        const result = await db.collection('userDetails').updateOne({ name }, updatedData);
-
-        if(result.modifiedCount > 0) {
-            res.json({ message: 'Record Updated' });
-        } else {
-            res.status(404).json({ message: 'Record Not Found or No Change Made' });
-        }
-    } catch(error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal Server Error' });
     }
-});
+       catch(error)
+       {
+        console.log(error);
+       }
+    
+   finally{
+    client.close()
 
-// Delete User by Name
-app.delete('/delete/:name', async(req, res)=>{
-    try {
-        const db = await connectDB();
-        const name = req.params.name;
-        const result = await db.collection('userDetails').deleteOne({ name });
 
-        if (result.deletedCount > 0) {
-            res.json({ message: 'Record Deleted' });
-        } else {
-            res.status(404).json({ message: 'Record Not Found' });
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal Server Error' });
+   }
+
+}) 
+app.put('/update/:num', async function(req,res)
+
+{
+    const client= await mongoClient.connect(dburl)
+    // const id =parseInt(req.params.num)
+    try
+    {
+       let db= await client.db('amity')
+       let user= await db.collection('userDetails').findOne({name:req.params.num})
+       if(user)
+       {
+
+        let data= await db.collection('userDetails').updateOne({name:req.params.num},{$set:{name:req.body.name}})
+       res.json({
+        message:'Record updated'
+        
+        
+       })
     }
-});
+    else{
+        res.json({
+            message:'record not found'
+        })
+    }
+    }
+       catch(error)
+       {
+        console.log(error);
+       }
+    
+   finally{
+    client.close()
 
-app.listen(8000, () => console.log('Server is running on port 8000'));
+
+   }
+
+}) 
+app.delete('/delete/:num', async function(req,res)
+
+{
+    const client= await mongoClient.connect(dburl)
+    // const id =parseInt(req.params.num)
+    try
+    {
+       let db= await client.db('amity')
+       let user= await db.collection('userDetails').findOne({name:req.params.num})
+       if(user)
+       {
+
+        let data= await db.collection('userDetails').deleteOne({name:req.params.num})
+       res.json({
+        message:'Record deleted'
+        
+        
+       })
+    }
+    else{
+        res.json({
+            message:'record not found'
+        })
+    }
+    }
+       catch(error)
+       {
+        console.log(error);
+       }
+    
+   finally{
+    client.close()
+
+
+   }
+
+}) 
+
+
+app.listen(8000, ()=>console.log('Server is running in the port 8000'))
